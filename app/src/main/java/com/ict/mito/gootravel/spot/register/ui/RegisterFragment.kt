ef061c4adf1076581329f5d0274b05ca9822f691
mitohato <mitohato14@gmail.com>
2019-07-23 22:25:33 +0900

    Add fragment for spot register

---
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/ui/RegisterFragment.kt
@@ -0,0 +1,37 @@
+package com.ict.mito.gootravel.spot.register.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.ict.mito.gootravel.R
+
+class RegisterFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = RegisterFragment()
+    }
+
+    private lateinit var viewModel: RegisterViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.register_fragment,
+            container,
+            false
+        )
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(RegisterViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/ui/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.ict.mito.gootravel.spot.register.ui
+
+import androidx.lifecycle.ViewModel;
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
