a95ade15b53d0dc5d340af93e9a3998487a3331e
mitohato <mitohato14@gmail.com>
2019-07-24 15:34:51 +0900

    Move dir

--- app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListRowItem.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListRowItem.kt
@@ -1,4 +1,4 @@
-package com.ict.mito.gootravel.spot.select.ui
+package com.ict.mito.gootravel.spot.select.list.ui
 
 /**
  * Created by mitohato14 on 2019-07-24.
--- app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListViewModel.kt
@@ -1,4 +1,4 @@
-package com.ict.mito.gootravel.spot.select.ui
+package com.ict.mito.gootravel.spot.select.list.ui
 
 import androidx.lifecycle.ViewModel
 
