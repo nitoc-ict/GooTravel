5cc4fa7ef25a9bf90c95e851b153e85718ad71c6
mitohato <mitohato14@gmail.com>
2019-07-24 15:39:43 +0900

    Add class for search

---
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/search/ui/SearchFragment.kt
@@ -0,0 +1,33 @@
+package com.ict.mito.gootravel.spot.select.search.ui
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.ict.mito.gootravel.R
+
+class SearchFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SearchFragment()
+    }
+
+    private lateinit var viewModel: SearchViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.search_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/search/ui/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.ict.mito.gootravel.spot.select.search.ui
+
+import androidx.lifecycle.ViewModel;
+
+class SearchViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
