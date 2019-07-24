b6cd3cc7ff2e8bf5e729b9c27c1c77fff4c91158
mitohato <mitohato14@gmail.com>
2019-07-24 14:17:55 +0900

    Add class for alert

---
+++ app/src/main/java/com/ict/mito/gootravel/disaster/model/NotificationModel.kt
@@ -0,0 +1,8 @@
+package com.ict.mito.gootravel.disaster.model
+
+/**
+ * Created by mitohato14 on 2019-07-24.
+ */
+data class NotificationModel(
+    val id: Int
+)
---
+++ app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
@@ -0,0 +1,7 @@
+package com.ict.mito.gootravel.disaster.notification.ui
+
+/**
+ * Created by mitohato14 on 2019-07-24.
+ */
+class NotificationAlertDialog {
+}
