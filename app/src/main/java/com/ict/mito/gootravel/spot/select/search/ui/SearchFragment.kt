c4eadee78b60d13d69bde81437890c9832cbad15
mitohato <mitohato14@gmail.com>
2019-07-25 22:33:41 +0900

    Use koin di in SearchFragment

--- app/src/main/java/com/ict/mito/gootravel/spot/select/search/ui/SearchFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/search/ui/SearchFragment.kt
@@ -5,14 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.ict.mito.gootravel.R
 import kotlinx.android.synthetic.main.search_fragment.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchFragment : Fragment() {
 
-    private lateinit var viewModel: SearchViewModel
+    private val viewModel: SearchViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,8 +24,6 @@ class SearchFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
-        // TODO: Use the ViewModel
         button_to_radar.setOnClickListener {
             findNavController().navigate(R.id.action_searchFragment_to_radarFragment)
         }
