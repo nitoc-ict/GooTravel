572b1bd142b851f99f02195b3a66b3f1db89f788
mitohato <mitohato14@gmail.com>
2019-07-25 21:57:16 +0900

    Add module for viewmodel

--- app/src/main/java/com/ict/mito/gootravel/App.kt
+++ app/src/main/java/com/ict/mito/gootravel/App.kt
@@ -1,8 +1,17 @@
 package com.ict.mito.gootravel
 
 import android.app.Application
+import com.ict.mito.gootravel.disaster.manual.ui.ManualViewModel
+import com.ict.mito.gootravel.spot.navigate.ui.NavigateViewModel
+import com.ict.mito.gootravel.spot.register.ui.RegisterViewModel
+import com.ict.mito.gootravel.spot.select.list.ui.ListViewModel
+import com.ict.mito.gootravel.spot.select.radar.ui.RadarViewModel
+import com.ict.mito.gootravel.spot.select.search.ui.SearchViewModel
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.dsl.module
 
 /**
  * Created by mitohato14 on 2019-07-25.
@@ -13,5 +22,14 @@ class App : Application() {
         startKoin {
             androidContext(this@App)
         }
+    }
+
+    private val viewModelModule: Module = module {
+        viewModel { ManualViewModel() }
+        viewModel { NavigateViewModel() }
+        viewModel { RegisterViewModel() }
+        viewModel { ListViewModel() }
+        viewModel { RadarViewModel() }
+        viewModel { SearchViewModel() }
     }
 }
