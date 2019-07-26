2957bd854723d4610701556ef70f197e63089a41
mitohato <mitohato14@gmail.com>
2019-07-26 13:36:29 +0900

    Set binding

--- app/src/main/java/com/ict/mito/gootravel/spot/navigate/ui/NavigateFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/navigate/ui/NavigateFragment.kt
@@ -9,14 +9,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.ict.mito.gootravel.R
+import com.ict.mito.gootravel.databinding.NavigateFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class NavigateFragment : Fragment(), LocationListener {
 
     private val viewModel: NavigateViewModel by viewModel()
     private var locationManager: LocationManager? = null
+    private var binding: NavigateFragmentBinding? = null
 
     @SuppressLint("MissingPermission")
     override fun onCreateView(
@@ -24,6 +27,16 @@ class NavigateFragment : Fragment(), LocationListener {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.navigate_fragment,
+            container,
+            false
+
+        )
+
+        binding?.viewmodel = viewModel
+
         locationManager = activity?.getSystemService(Activity.LOCATION_SERVICE) as LocationManager
         locationManager?.requestLocationUpdates(
             LocationManager.GPS_PROVIDER,
@@ -31,11 +44,7 @@ class NavigateFragment : Fragment(), LocationListener {
             50f,
             this
         )
-        return inflater.inflate(
-            R.layout.navigate_fragment,
-            container,
-            false
-        )
+        return binding?.root
     }
 
     override fun onLocationChanged(location: Location?) {
