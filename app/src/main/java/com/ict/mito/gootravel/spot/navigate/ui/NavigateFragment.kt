60202e9fbfc2a3b68e290749ce701ef72c7bff3e
mitohato <mitohato14@gmail.com>
2019-07-26 13:28:11 +0900

    Instance and setting location manager

--- app/src/main/java/com/ict/mito/gootravel/spot/navigate/ui/NavigateFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/navigate/ui/NavigateFragment.kt
@@ -1,6 +1,10 @@
 package com.ict.mito.gootravel.spot.navigate.ui
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.location.Location
 import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,12 +16,21 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class NavigateFragment : Fragment(), LocationListener {
 
     private val viewModel: NavigateViewModel by viewModel()
+    private var locationManager: LocationManager? = null
 
+    @SuppressLint("MissingPermission")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        locationManager = activity?.getSystemService(Activity.LOCATION_SERVICE) as LocationManager
+        locationManager?.requestLocationUpdates(
+            LocationManager.GPS_PROVIDER,
+            1000L,
+            50f,
+            this
+        )
         return inflater.inflate(
             R.layout.navigate_fragment,
             container,
