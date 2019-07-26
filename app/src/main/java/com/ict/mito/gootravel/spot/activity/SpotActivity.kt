5f202d236fbb8f177667a1eca5f99e8cbd4ff1d4
mitohato <mitohato14@gmail.com>
2019-07-26 13:09:15 +0900

    Request permission to use location

--- app/src/main/java/com/ict/mito/gootravel/spot/activity/SpotActivity.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/activity/SpotActivity.kt
@@ -1,6 +1,10 @@
 package com.ict.mito.gootravel.spot.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.ict.mito.gootravel.R
 
@@ -9,5 +13,26 @@ class SpotActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spot)
+
+        // API 23 or more?
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermission()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun checkPermission() {
+        if (
+            checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                arrayOf(
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                0
+            )
+        }
     }
 }
