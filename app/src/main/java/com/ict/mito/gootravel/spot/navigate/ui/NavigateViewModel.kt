cb8a2fbc5c2c2fa847bf9a34a8893fbd92b2b1dd
mitohato <mitohato14@gmail.com>
2019-07-26 14:10:07 +0900

    Make viewmodel para to LiveData

--- app/src/main/java/com/ict/mito/gootravel/spot/navigate/ui/NavigateViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/navigate/ui/NavigateViewModel.kt
@@ -1,8 +1,9 @@
 package com.ict.mito.gootravel.spot.navigate.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class NavigateViewModel : ViewModel() {
-    var latitude: Double = 0.0
-    var longitude: Double = 0.0
+    var latitude: MutableLiveData<String> = MutableLiveData()
+    var longitude: MutableLiveData<String> = MutableLiveData()
 }
