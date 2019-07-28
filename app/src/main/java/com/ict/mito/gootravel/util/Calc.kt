179daacadee5d65106f8313a46a60267a3141d59
mitohato <mitohato14@gmail.com>
2019-07-28 13:33:38 +0900

    Impl distance logical

--- app/src/main/java/com/ict/mito/gootravel/util/Calc.kt
+++ app/src/main/java/com/ict/mito/gootravel/util/Calc.kt
@@ -1,5 +1,10 @@
 package com.ict.mito.gootravel.util
 
+import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
+
 /**
  * Created by mitohato14 on 2019-07-28.
  */
@@ -11,5 +16,19 @@ class Calc {
         bx: Double,
         by: Double
     ): Double {
+        val P: Double = (ax + ay) / 2.0
+        val W: Double = sqrt(1 - (E * E) * (sin(P) * sin(P)))
+        val M: Double = (Rx * (1 - (E * E))) / (W * W * W)
+        val N: Double = Rx / M
+        val dx = abs(ax - bx)
+        val dy = abs(ay - by)
+        val D = sqrt((dy * M) * (dy * M) + (dx * N * cos(P)) * (dx * N * cos(P)))
+
+        return D
+    }
+    companion object {
+        private const val Rx: Double = 6_378_137.000
+        private const val Ry: Double = 6_356_752.314_245
+        private val E: Double = sqrt(((Rx * Rx) - (Ry * Ry)) / (Rx * Rx))
     }
 }
