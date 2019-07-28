e9c0996d5294425a368669750a56998aa03ca17f
mitohato <mitohato14@gmail.com>
2019-07-28 14:48:55 +0900

    Set observer MediatorLiveData

--- app/src/main/java/com/ict/mito/gootravel/spot/navigate/ui/NavigateViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/navigate/ui/NavigateViewModel.kt
@@ -1,9 +1,28 @@
 package com.ict.mito.gootravel.spot.navigate.ui
 
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import com.ict.mito.gootravel.util.Calc
 
 class NavigateViewModel : ViewModel() {
     var latitude: MutableLiveData<String> = MutableLiveData()
     var longitude: MutableLiveData<String> = MutableLiveData()
+    var distance: MediatorLiveData<String> = MediatorLiveData()
+
+    init {
+        val observer = Observer<String> {
+            val latitudeNonNull = latitude.value?.toDouble() ?: 0.0
+            val longtideNonNull = latitude.value?.toDouble() ?: 0.0
+            distance.value = Calc().directDistance(
+                latitudeNonNull,
+                longtideNonNull,
+                0.0,
+                0.0
+            ).toString()
+        }
+        distance.addSource(latitude, observer)
+        distance.addSource(longitude, observer)
+    }
 }
