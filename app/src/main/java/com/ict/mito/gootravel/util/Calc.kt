b37b182de22eb88ed842edf92f060a0bcede4fe1
mitohato <mitohato14@gmail.com>
2019-07-28 18:30:00 +0900

    Impl way to calc direction

--- app/src/main/java/com/ict/mito/gootravel/util/Calc.kt
+++ app/src/main/java/com/ict/mito/gootravel/util/Calc.kt
@@ -1,5 +1,10 @@
 package com.ict.mito.gootravel.util
 
+import kotlin.math.atan
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.tan
+
 /**
  * Created by mitohato14 on 2019-07-28.
  */
@@ -11,5 +16,7 @@ class Calc {
         bx: Double,
         by: Double
     ): Double {
+        return (90 - atan(2 * ((sin((bx - ax))) / (cos(ay) * tan(by) - sin((ay) * cos(bx - ax))))))
     }
+
 }
