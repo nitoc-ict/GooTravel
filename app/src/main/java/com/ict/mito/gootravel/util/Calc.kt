e1f58a04963e9833cd63e8088e0c43e34040eb8d
mitohato <mitohato14@gmail.com>
2019-07-28 11:05:08 +0900

    Add method

--- app/src/main/java/com/ict/mito/gootravel/util/Calc.kt
+++ app/src/main/java/com/ict/mito/gootravel/util/Calc.kt
@@ -4,5 +4,12 @@ package com.ict.mito.gootravel.util
  * Created by mitohato14 on 2019-07-28.
  */
 class Calc {
-    
+
+    fun directDistance(
+        ax: Double,
+        ay: Double,
+        bx: Double,
+        by: Double
+    ): Double {
+    }
 }
