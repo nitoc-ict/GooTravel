b840997071da4dea611b9df5b59ac1669464d456
mitohato <mitohato14@gmail.com>
2019-07-28 19:16:30 +0900

    Change way to calc direction

--- app/src/main/java/com/ict/mito/gootravel/util/Calc.kt
+++ app/src/main/java/com/ict/mito/gootravel/util/Calc.kt
@@ -1,6 +1,6 @@
 package com.ict.mito.gootravel.util
 
-import kotlin.math.*
+import android.location.Location
 
 /**
  * Created by mitohato14 on 2019-07-28.
@@ -11,18 +11,13 @@ fun calcDirectDistance(
     bx: Double,
     by: Double
 ): Double {
-    val P: Double = (ax + bx) / 2.0
-    val W: Double = sqrt(1 - ((E * E) * (sin(P) * sin(P))))
-    val M: Double = (Rx * (1 - (E * E))) / (W * W * W)
-    val N: Double = Rx / W
-    val dx = abs(max(ax, bx) - min(ax, bx))
-    val dy = abs(max(ay, by) - min(ay, by))
-    val D = sqrt((dy * M) * (dy * M) + (dx * N * cos(P)) * (dx * N * cos(P)))
-//
-    return D
-//        return (sqrt(dx * dx + dy * dy) * 100)
+   val results = FloatArray(3)
+    Location.distanceBetween(
+        ax,
+        ay,
+        bx,
+        by,
+        results
+    )
+    return results[0].toDouble()
 }
-
-private const val Rx: Double = 6_378_137.000
-private const val Ry: Double = 6_356_752.314_245
-private val E: Double = sqrt(((Rx * Rx) - (Ry * Ry)) / (Rx * Rx))
