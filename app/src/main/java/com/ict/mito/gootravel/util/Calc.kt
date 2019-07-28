6fa8ffee89f46a665b458499d7f4db676356e28c
mitohato <mitohato14@gmail.com>
2019-07-28 18:29:54 +0900

    Add method

--- app/src/main/java/com/ict/mito/gootravel/util/Calc.kt
+++ app/src/main/java/com/ict/mito/gootravel/util/Calc.kt
@@ -4,4 +4,12 @@ package com.ict.mito.gootravel.util
  * Created by mitohato14 on 2019-07-28.
  */
 class Calc {
+
+    fun direction(
+        ax: Double,
+        ay: Double,
+        bx: Double,
+        by: Double
+    ): Double {
+    }
 }
