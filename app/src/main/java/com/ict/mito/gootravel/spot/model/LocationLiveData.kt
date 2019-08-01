ed31b27a75f276779c78af0a6487b136686e08e3
mitohato <mitohato14@gmail.com>
2019-08-01 22:23:59 +0900

    Add logic for location

--- app/src/main/java/com/ict/mito/gootravel/spot/model/LocationLiveData.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/model/LocationLiveData.kt
@@ -1,11 +1,16 @@
 package com.ict.mito.gootravel.spot.model
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import androidx.lifecycle.LiveData
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import timber.log.Timber
 
 /**
@@ -30,6 +35,54 @@ class LocationLiveData(
         override fun onConnectionSuspended(p0: Int) {
             Timber.d("onConnectionSuspended")
             googleApiClient.connect()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onActive() {
+        super.onActive()
+        googleApiClient = GoogleApiClient.Builder(context!!)
+            .addConnectionCallbacks(googleApiClientConnectionCallbacks)
+            .addOnConnectionFailedListener(googleConnectionFailedListener)
+            .addApi(LocationServices.API)
+            .build()
+
+        createLocationRequest()
+        val locationClient = LocationServices.getFusedLocationProviderClient(context!!)
+        locationClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback() {
+                override fun onLocationResult(p0: LocationResult?) {
+                    super.onLocationResult(p0)
+                    p0?.let { updateLocationInfo(it.lastLocation) }
+                }
+            },
+            Looper.myLooper()
+        )
+        locationClient.lastLocation.addOnSuccessListener {
+            updateLocationInfo(it)
+        }
+
+        googleApiClient.connect()
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+        googleApiClient.disconnect()
+    }
+
+    private fun updateLocationInfo(location: Location) {
+        Timber.d("latitude:${location.latitude}")
+        Timber.d("longitude:${location.longitude}")
+        value = location
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.also {
+            it.interval = 1000
+            it.fastestInterval = 500
+            it.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
     }
 
