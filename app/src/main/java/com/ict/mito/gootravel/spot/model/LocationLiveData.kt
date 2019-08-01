3fae35c83c3fbaccc9ec0c062fa1fc2573638785
mitohato <mitohato14@gmail.com>
2019-08-01 22:17:14 +0900

    Add need var

--- app/src/main/java/com/ict/mito/gootravel/spot/model/LocationLiveData.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/model/LocationLiveData.kt
@@ -1,10 +1,33 @@
 package com.ict.mito.gootravel.spot.model
 
 import android.location.Location
+import android.os.Bundle
 import androidx.lifecycle.LiveData
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationRequest
+import timber.log.Timber
 
 /**
  * Created by mitohato14 on 2019-08-01.
  */
 class LocationLiveData : LiveData<Location>() {
+    private lateinit var googleApiClient: GoogleApiClient
+    private lateinit var locationRequest: LocationRequest
+
+    private val googleConnectionFailedListener = GoogleApiClient.OnConnectionFailedListener {
+        Timber.d("ConnectionFailed")
+        Timber.d(it.errorCode.toString())
+    }
+
+    private val googleApiClientConnectionCallbacks = object : GoogleApiClient.ConnectionCallbacks {
+        override fun onConnected(p0: Bundle?) {
+            Timber.d("onConnected")
+        }
+
+        override fun onConnectionSuspended(p0: Int) {
+            Timber.d("onConnectionSuspended")
+            googleApiClient.connect()
+        }
+    }
+
 }
