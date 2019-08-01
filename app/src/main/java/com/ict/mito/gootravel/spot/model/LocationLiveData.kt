57bfbd5ad9fe98d55944e7a3880b39d3c573564c
mitohato <mitohato14@gmail.com>
2019-08-01 22:16:19 +0900

    Add livedata class for location

---
+++ app/src/main/java/com/ict/mito/gootravel/spot/model/LocationLiveData.kt
@@ -0,0 +1,10 @@
+package com.ict.mito.gootravel.spot.model
+
+import android.location.Location
+import androidx.lifecycle.LiveData
+
+/**
+ * Created by mitohato14 on 2019-08-01.
+ */
+class LocationLiveData : LiveData<Location>() {
+}
