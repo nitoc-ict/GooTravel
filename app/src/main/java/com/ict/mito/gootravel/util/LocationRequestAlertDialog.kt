a72e4d96ec19098baae8005f36e36516119466a4
mitohato <mitohato14@gmail.com>
2019-08-19 11:45:52 +0900

    Create dialog logic

--- app/src/main/java/com/ict/mito/gootravel/util/LocationRequestAlertDialog.kt
+++ app/src/main/java/com/ict/mito/gootravel/util/LocationRequestAlertDialog.kt
@@ -1,11 +1,23 @@
 package com.ict.mito.gootravel.util
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 
 /**
  * Created by mitohato14 on 2019-08-19.
  */
 class LocationRequestAlertDialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return AlertDialog.Builder(activity)
+            .setTitle("")
+            .setMessage("")
+            .setPositiveButton("") { _, _ ->
+                activity?.finish()
+            }
+            .create()
+    }
 
     override fun onPause() {
         super.onPause()
