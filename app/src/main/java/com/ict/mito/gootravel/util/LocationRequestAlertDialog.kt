fd5271ecb53ab78315af92868bd9efd0f4d8615b
mitohato <mitohato14@gmail.com>
2019-08-19 11:05:32 +0900

    Add parent

--- app/src/main/java/com/ict/mito/gootravel/util/LocationRequestAlertDialog.kt
+++ app/src/main/java/com/ict/mito/gootravel/util/LocationRequestAlertDialog.kt
@@ -1,7 +1,9 @@
 package com.ict.mito.gootravel.util
 
+import androidx.fragment.app.DialogFragment
+
 /**
  * Created by mitohato14 on 2019-08-19.
  */
-class LocationRequestAlertDialog {
+class LocationRequestAlertDialog : DialogFragment() {
 }
