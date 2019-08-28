004a94f216234f48410aef90f62f12662222cf6e
mitohato <mitohato14@gmail.com>
2019-08-28 21:59:27 +0900

    Add entity annotation

--- app/src/main/java/com/ict/mito/gootravel/db/RoomRegisterLocation.kt
+++ app/src/main/java/com/ict/mito/gootravel/db/RoomRegisterLocation.kt
@@ -1,7 +1,10 @@
 package com.ict.mito.gootravel.db
 
+import androidx.room.Entity
+
 /**
  * Created by mitohato14 on 2019-08-28.
  */
+@Entity(tableName = "gootravel_table")
 class RoomRegisterLocation {
 }
