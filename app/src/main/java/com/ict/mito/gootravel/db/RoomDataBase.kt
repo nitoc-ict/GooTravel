cdde534c9ee1e759c8305a4c8c6cf202f67cf024
mitohato <mitohato14@gmail.com>
2019-08-28 22:16:51 +0900

    Add database annotation

--- app/src/main/java/com/ict/mito/gootravel/db/RoomDataBase.kt
+++ app/src/main/java/com/ict/mito/gootravel/db/RoomDataBase.kt
@@ -1,7 +1,14 @@
 package com.ict.mito.gootravel.db
 
+import androidx.room.Database
+
 /**
  * Created by mitohato14 on 2019-08-28.
  */
+@Database(
+    entities = [RoomRegisterLocation::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class RoomDataBase {
 }
