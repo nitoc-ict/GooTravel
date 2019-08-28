a5e23947888baff23571ca529f6030905cfff0aa
mitohato <mitohato14@gmail.com>
2019-08-28 22:24:57 +0900

    Add dao method

--- app/src/main/java/com/ict/mito/gootravel/db/RegisterDataRoomDataBase.kt
+++ app/src/main/java/com/ict/mito/gootravel/db/RegisterDataRoomDataBase.kt
@@ -2,6 +2,7 @@ package com.ict.mito.gootravel.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.ict.mito.gootravel.db.dao.RegisterDataDAO
 
 /**
  * Created by mitohato14 on 2019-08-28.
@@ -12,4 +13,5 @@ import androidx.room.RoomDatabase
     exportSchema = false
 )
 abstract class RegisterDataRoomDataBase : RoomDatabase() {
+    abstract fun dao(): RegisterDataDAO
 }
