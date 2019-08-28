618101204ffb18572753da123e8f0aa73c8935f7
mitohato <mitohato14@gmail.com>
2019-08-28 22:25:56 +0900

    Add companion object

--- app/src/main/java/com/ict/mito/gootravel/db/RegisterDataRoomDataBase.kt
+++ app/src/main/java/com/ict/mito/gootravel/db/RegisterDataRoomDataBase.kt
@@ -1,6 +1,8 @@
 package com.ict.mito.gootravel.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.ict.mito.gootravel.db.dao.RegisterDataDAO
 
@@ -14,4 +16,25 @@ import com.ict.mito.gootravel.db.dao.RegisterDataDAO
 )
 abstract class RegisterDataRoomDataBase : RoomDatabase() {
     abstract fun dao(): RegisterDataDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: RegisterDataRoomDataBase? = null
+
+        fun getDataBase(context: Context): RegisterDataRoomDataBase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    RegisterDataRoomDataBase::class.java,
+                    "gootravel_db"
+                )
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
