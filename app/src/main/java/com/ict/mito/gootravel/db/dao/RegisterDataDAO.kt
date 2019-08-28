521c90e12ac52e6c6018673d34895bc684a6dcd5
mitohato <mitohato14@gmail.com>
2019-08-28 22:22:22 +0900

    Add insert

--- app/src/main/java/com/ict/mito/gootravel/db/dao/RegisterDataDAO.kt
+++ app/src/main/java/com/ict/mito/gootravel/db/dao/RegisterDataDAO.kt
@@ -1,10 +1,15 @@
 package com.ict.mito.gootravel.db.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.ict.mito.gootravel.db.RoomRegisterLocation
 
 /**
  * Created by mitohato14 on 2019-08-28.
  */
 @Dao
 interface RegisterDataDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(location: RoomRegisterLocation)
 }
