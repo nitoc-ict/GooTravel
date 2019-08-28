13a70a7c6b1ecfb93e555a0ff9aac7c0eed3f3a7
mitohato <mitohato14@gmail.com>
2019-08-28 22:15:09 +0900

    Add convert method

--- app/src/main/java/com/ict/mito/gootravel/db/DataBaseConverter.kt
+++ app/src/main/java/com/ict/mito/gootravel/db/DataBaseConverter.kt
@@ -1,7 +1,19 @@
 package com.ict.mito.gootravel.db
 
+import com.ict.mito.gootravel.spot.model.RegisterPointData
+
 /**
  * Created by mitohato14 on 2019-08-28.
  */
 class DataBaseConverter {
+    fun convert2RoomRegisterLocation(registerPointData: RegisterPointData): RoomRegisterLocation {
+        return RoomRegisterLocation(
+            "",
+            0.0,
+            0.0,
+            0.0,
+            ByteArray(0),
+            ""
+        )
+    }
 }
