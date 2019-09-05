bd21d959a4396acf2119bcb1c6be894c824062fa
mitohato <mitohato14@gmail.com>
2019-09-05 13:37:11 +0900

    Add livedata

--- app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dailog/SelectSpotBottomSheetViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dailog/SelectSpotBottomSheetViewModel.kt
@@ -1,9 +1,15 @@
 package com.ict.mito.gootravel.spot.select.radar.ui.dailog
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ict.mito.gootravel.spot.model.SpotData
 
 /**
  * Created by mitohato14 on 2019-09-05.
  */
 class SelectSpotBottomSheetViewModel : ViewModel() {
+    private val _spotData: MutableLiveData<SpotData> = MutableLiveData()
+    val spotData: LiveData<SpotData>
+        get() = _spotData
 }
