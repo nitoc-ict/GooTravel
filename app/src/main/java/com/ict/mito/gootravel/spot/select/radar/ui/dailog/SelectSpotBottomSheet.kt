41241b611201deda31ba67f49bf9436d6b25b3e4
mitohato <mitohato14@gmail.com>
2019-09-05 11:44:13 +0900

    Add bottom sheet dialog fragment class

---
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dailog/SelectSpotBottomSheet.kt
@@ -0,0 +1,9 @@
+package com.ict.mito.gootravel.spot.select.radar.ui.dailog
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**
+ * Created by mitohato14 on 2019-09-05.
+ */
+class SelectSpotBottomSheet : BottomSheetDialogFragment() {
+}
