a31bb00dcb6dec77bd579628f09eef852efe4bbd
mitohato <mitohato14@gmail.com>
2019-09-05 15:20:55 +0900

    Add method what read spot data

--- app/src/main/java/com/ict/mito/gootravel/repo/ReadRepository.kt
+++ app/src/main/java/com/ict/mito/gootravel/repo/ReadRepository.kt
@@ -1,7 +1,13 @@
 package com.ict.mito.gootravel.repo
 
+import com.ict.mito.gootravel.spot.model.SpotData
+import io.reactivex.Maybe
+import io.reactivex.Single
+
 /**
  * Created by mitohato14 on 2019-09-05.
  */
 interface ReadRepository {
+    fun getSpotDataById(id: Long): Maybe<SpotData>
+    fun getAllSpotData(): Single<List<SpotData>>
 }
