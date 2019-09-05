38a634d3e5ae1317270a3735856ce633088e09a2
mitohato <mitohato14@gmail.com>
2019-09-05 11:59:07 +0900

    Add binding

--- app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dailog/SelectSpotBottomSheet.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dailog/SelectSpotBottomSheet.kt
@@ -4,22 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.ict.mito.gootravel.R
+import com.ict.mito.gootravel.databinding.SelectSpotPopupBinding
 
 /**
  * Created by mitohato14 on 2019-09-05.
  */
 class SelectSpotBottomSheet : BottomSheetDialogFragment() {
+    private var binding: SelectSpotPopupBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(
+        binding = DataBindingUtil.inflate(
+            inflater,
             R.layout.select_spot_popup,
             container,
             false
         )
+
+        return binding?.root
     }
 }
