fe77582b4bfa648bace18840ce7ae4cabbce0978
mitohato <mitohato14@gmail.com>
2019-09-05 13:26:52 +0900

    Extend viewmodel

--- app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dailog/SelectSpotBottomSheetViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dailog/SelectSpotBottomSheetViewModel.kt
@@ -1,7 +1,9 @@
 package com.ict.mito.gootravel.spot.select.radar.ui.dailog
 
+import androidx.lifecycle.ViewModel
+
 /**
  * Created by mitohato14 on 2019-09-05.
  */
-class SelectSpotBottomSheetViewModel {
+class SelectSpotBottomSheetViewModel : ViewModel() {
 }
