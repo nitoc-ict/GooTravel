5253808863782404e62585cf3fe944562f7300ea
mitohato <mitohato14@gmail.com>
2019-09-06 12:53:14 +0900

    Rename dao

--- app/src/main/java/com/ict/mito/gootravel/repo/impl/RepositoryImpl.kt
+++ app/src/main/java/com/ict/mito/gootravel/repo/impl/RepositoryImpl.kt
@@ -10,20 +10,20 @@ import io.reactivex.Single
 /**
  * Created by mitohato14 on 2019-09-05.
  */
-class RepositoryImpl(private val dao: RegisterDataDAO) : Repository {
-    override fun add(spotData: SpotData) = dao.insert(spotData)
+class RepositoryImpl(private val registerDataDAO: RegisterDataDAO) : Repository {
+    override fun add(spotData: SpotData) = registerDataDAO.insert(spotData)
 
-    override fun add(roomRegisterLocation: RoomRegisterLocation) = dao.insert(roomRegisterLocation)
+    override fun add(roomRegisterLocation: RoomRegisterLocation) = registerDataDAO.insert(roomRegisterLocation)
 
     override fun getSpotDataById(id: Long): Maybe<SpotData> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getAllSpotData(): Single<List<SpotData>> = dao.getAllSpotData()
+    override fun getAllSpotData(): Single<List<SpotData>> = registerDataDAO.getAllSpotData()
 
     override fun getRegisterLocationById(id: Long): Maybe<RoomRegisterLocation> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun getAllRegisterLocation(): Single<List<RoomRegisterLocation>> = dao.getAllRegister()
+    override fun getAllRegisterLocation(): Single<List<RoomRegisterLocation>> = registerDataDAO.getAllRegister()
 }
