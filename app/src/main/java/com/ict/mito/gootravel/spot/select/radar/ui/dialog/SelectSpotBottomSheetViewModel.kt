33b6b65c63ad2d86ba74bc018994b97187ce3cc8
mitohato <mitohato14@gmail.com>
2019-09-06 15:52:11 +0900

    Set repo constructor

--- app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dialog/SelectSpotBottomSheetViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dialog/SelectSpotBottomSheetViewModel.kt
@@ -3,12 +3,13 @@ package com.ict.mito.gootravel.spot.select.radar.ui.dialog
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ict.mito.gootravel.repo.Repository
 import com.ict.mito.gootravel.spot.model.SpotData
 
 /**
  * Created by mitohato14 on 2019-09-05.
  */
-class SelectSpotBottomSheetViewModel : ViewModel() {
+class SelectSpotBottomSheetViewModel(repository: Repository) : ViewModel() {
     private val _spotData: MutableLiveData<SpotData> = MutableLiveData()
     val spotData: LiveData<SpotData>
         get() = _spotData
