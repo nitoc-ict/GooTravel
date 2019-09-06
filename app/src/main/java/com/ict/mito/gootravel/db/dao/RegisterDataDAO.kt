76191be2ba309e8abc3098c176e883c5f3d19b32
mitohato <mitohato14@gmail.com>
2019-09-06 12:50:13 +0900

    Move dao method

--- app/src/main/java/com/ict/mito/gootravel/db/dao/RegisterDataDAO.kt
+++ app/src/main/java/com/ict/mito/gootravel/db/dao/RegisterDataDAO.kt
@@ -5,7 +5,6 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.ict.mito.gootravel.db.RoomRegisterLocation
-import com.ict.mito.gootravel.spot.model.SpotData
 import io.reactivex.Single
 
 /**
@@ -18,10 +17,4 @@ interface RegisterDataDAO {
 
     @Query("SELECT * FROM gootravel_table")
     fun getAllRegister(): Single<List<RoomRegisterLocation>>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(spotData: SpotData)
-
-    @Query("SELECT * FROM spotdata")
-    fun getAllSpotData(): Single<List<SpotData>>
 }
--- app/src/main/java/com/ict/mito/gootravel/db/dao/SpotDataDAO.kt
+++ app/src/main/java/com/ict/mito/gootravel/db/dao/SpotDataDAO.kt
@@ -1,10 +1,20 @@
 package com.ict.mito.gootravel.db.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.ict.mito.gootravel.spot.model.SpotData
+import io.reactivex.Single
 
 /**
  * Created by mitohato14 on 2019-09-06.
  */
 @Dao
 interface SpotDataDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(spotData: SpotData)
+
+    @Query("SELECT * FROM spotdata")
+    fun getAllSpotData(): Single<List<SpotData>>
 }
