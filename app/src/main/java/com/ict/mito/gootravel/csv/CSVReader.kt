c6317e0ebfd4198e22024ef014837be784cb25bc
mitohato <mitohato14@gmail.com>
2019-09-09 22:16:44 +0900

    Add method

--- app/src/main/java/com/ict/mito/gootravel/csv/CSVReader.kt
+++ app/src/main/java/com/ict/mito/gootravel/csv/CSVReader.kt
@@ -1,7 +1,13 @@
 package com.ict.mito.gootravel.csv
 
+import com.ict.mito.gootravel.spot.model.SpotData
+import io.reactivex.Single
+
 /**
  * Created by mitohato14 on 2019-09-09.
  */
 class CSVReader {
+    fun getSpotDataList(): Single<List<SpotData>> = Single.create {
+        
+    }
 }
