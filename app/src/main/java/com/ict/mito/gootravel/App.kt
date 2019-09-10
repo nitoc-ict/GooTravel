0ac326db90e55feb1f457c4c6e5892600c1a7817
mitohato <mitohato14@gmail.com>
2019-09-10 13:24:35 +0900

    Add reader module

--- app/src/main/java/com/ict/mito/gootravel/App.kt
+++ app/src/main/java/com/ict/mito/gootravel/App.kt
@@ -1,6 +1,7 @@
 package com.ict.mito.gootravel
 
 import android.app.Application
+import com.ict.mito.gootravel.csv.CSVReader
 import com.ict.mito.gootravel.db.GooTravelDataRoomDataBase
 import com.ict.mito.gootravel.disaster.manual.ui.ManualViewModel
 import com.ict.mito.gootravel.repo.Repository
@@ -71,5 +72,9 @@ class App : Application() {
                 get()
             ) as Repository
         }
+    }
+
+    private val readerModule: Module = module {
+        single { CSVReader(applicationContext) }
     }
 }
