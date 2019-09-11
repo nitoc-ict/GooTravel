5479bb06d426f2f480eab1a44b0673dc047993d0
mitohato <mitohato14@gmail.com>
2019-09-11 16:55:01 +0900

    Lint

--- app/src/main/java/com/ict/mito/gootravel/csv/CSVReader.kt
+++ app/src/main/java/com/ict/mito/gootravel/csv/CSVReader.kt
@@ -6,7 +6,7 @@ import io.reactivex.Single
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.Reader
-import java.util.*
+import java.util.StringTokenizer
 
 /**
  * Created by mitohato14 on 2019-09-09.
