65d02b89c8c4f15736730e3230d9721f07c13673
mitohato <mitohato14@gmail.com>
2019-09-11 16:20:55 +0900

    Use and Add string value

--- app/src/main/java/com/ict/mito/gootravel/util/LocationRequestAlertDialog.kt
+++ app/src/main/java/com/ict/mito/gootravel/util/LocationRequestAlertDialog.kt
@@ -4,6 +4,7 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import com.ict.mito.gootravel.R
 
 /**
  * Created by mitohato14 on 2019-08-19.
@@ -11,8 +12,8 @@ import androidx.fragment.app.DialogFragment
 class LocationRequestAlertDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return AlertDialog.Builder(activity)
-            .setTitle("")
-            .setMessage("")
+            .setTitle(getString(R.string.location_request_dialog_title))
+            .setMessage(getString(R.string.location_request_dialog_body))
             .setPositiveButton("") { _, _ ->
                 activity?.finish()
             }
