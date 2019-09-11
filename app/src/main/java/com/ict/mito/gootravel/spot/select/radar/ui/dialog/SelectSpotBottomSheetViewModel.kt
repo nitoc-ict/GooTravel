b7a3d7531a74df9aff8ff464fbdc3f355b48f480
mitohato <mitohato14@gmail.com>
2019-09-11 15:35:35 +0900

    Commnet out

--- app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dialog/SelectSpotBottomSheetViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/dialog/SelectSpotBottomSheetViewModel.kt
@@ -17,8 +17,8 @@ class SelectSpotBottomSheetViewModel(private val repository: Repository) : ViewM
     val distance: String = "0m"
 
     fun setId(id: Long) {
-        repository.getSpotDataById(id).map {
-            _spotData.postValue(it)
-        }.subscribe()
+//        repository.getSpotDataById(id).map {
+//            _spotData.postValue(it)
+//        }.subscribe()
     }
 }
