3c214b7ca3abf360b5514955caab45b3a6d671c9
tapokaφ <52156547+Tapioka10315858@users.noreply.github.com>
2019-09-12 15:49:33 +0900

    修正箇所をなおしてエラー出てるところをimportした

--- app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
+++ app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
@@ -1,15 +1,16 @@
-class TestDialogFragment:DialogFragment() {
-        fun onCreateDialog(savedInstanceState:Bundle?):Dialog {
-            val builder = AlertDialog.Builder(getActivity())
-            builder.setTitle("Earthquake")
-                .setMessage("massage")
-                .setIcon(R.drawable.error)
-                .setPositiveButton("REFUGE", object: DialogInterface.OnClickListener {
-                    override fun onClick(dialog:DialogInterface, id:Int) {}
-                })
-                .setNegativeButton("MANUAL", object: DialogInterface.OnClickListener {
-                    override fun onClick(dialog:DialogInterface, id:Int) {}
-                })
-            return builder.create()
-        }
+package com.ict.mito.gootravel.disaster.notification.ui
+class NotificationAlertDialog.kt:DialogFragment() {
+override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(getActivity())
+        builder.setTitle("Earthquake")
+            .setMessage("massage")
+            .setIcon(R.drawable.error)
+            .setPositiveButton("REFUGE", object: DialogInterface.OnClickListener {
+                override fun onClick(dialog:DialogInterface, id:Int) {}
+            })
+            .setNegativeButton("MANUAL", object: DialogInterface.OnClickListener {
+                override fun onClick(dialog:DialogInterface, id:Int) {}
+            })
+        return builder.create()
+    }
 }
