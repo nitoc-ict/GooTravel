82ee1095b8d1dd058952f5bc2acffb5fbea3c6db
tapokaφ <52156547+Tapioka10315858@users.noreply.github.com>
2019-09-12 16:58:14 +0900

    importしました

--- app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
+++ app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
@@ -1,10 +1,17 @@
 package com.ict.mito.gootravel.disaster.notification.ui
-class NotificationAlertDialog:DialogFragment() {
-override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val builder = AlertDialog.Builder(getActivity())
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.example.myapplica.R
+
+class NotificationAlertDialog: DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
         builder.setTitle("Earthquake")
             .setMessage("massage")
-            .setIcon(R.drawable.error)
+            .setIcon(R.mipmap.ic_launcher)
             .setPositiveButton("REFUGE", object: DialogInterface.OnClickListener {
                 override fun onClick(dialog:DialogInterface, id:Int) {}
             })
