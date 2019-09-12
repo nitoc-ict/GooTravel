3314a9435a3e19c4abf3bba0ec269c6715ab21f2
tapioka10315858 <tapiokamai10315858@gmail.com>
2019-09-12 11:29:11 +0900

    Wi-fi画面のダイアログを書いた

--- app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
+++ app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
@@ -1,6 +1,30 @@
 package com.ict.mito.gootravel.disaster.notification.ui
 
-/**
- * Created by mitohato14 on 2019-07-24.
- */
-class NotificationAlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+class MainActivity:AppCompatActivity() {
+    protected fun onCreate(savedInstanceState:Bundle) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment)
+        val newFragment = TestDialogFragment()
+        newFragment.show(getSupportFragmentManager(), "test")
+    }
+    class TestDialogFragment:DialogFragment() {
+        fun onCreateDialog(savedInstanceState:Bundle):Dialog {
+            val builder = AlertDialog.Builder(getActivity())
+            builder.setTitle("SSID")
+                    .setMessage("massage")
+                    .setPositiveButton("CONNECT", object:DialogInterface.OnClickListener() {
+                        fun onClick(dialog:DialogInterface, id:Int) {}
+                    })
+                    .setNegativeButton("CANCEL", object:DialogInterface.OnClickListener() {
+                        fun onClick(dialog:DialogInterface, id:Int) {}
+                    })
+            return builder.create()
+        }
+    }
+}
