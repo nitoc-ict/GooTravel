f8e7484b3a0424e2182295877960947cecef5937
tapioka10315858 <tapiokamai10315858@gmail.com>
2019-09-12 14:00:15 +0900

    disasterDaialog

--- app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
+++ app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
@@ -1,6 +1,31 @@
 package com.ict.mito.gootravel.disaster.notification.ui
 
-/**
- * Created by mitohato14 on 2019-07-24.
- */
-class NotificationAlertDialog
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+class MainActivity:AppCompatActivity() {
+    protected fun onCreate(savedInstanceState:Bundle) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment)
+        val newFragment = TestDialogFragment()
+        newFragment.show(getSupportFragmentManager(), "test")
+    }
+    class TestDialogFragment:DialogFragment() {
+        fun onCreateDialog(savedInstanceState:Bundle):Dialog {
+            val builder = AlertDialog.Builder(getActivity())
+            builder.setTitle("Earthquake")
+                .setMessage("massage")
+                .setIcon(R.drawable.error)
+                .setPositiveButton("REFUGE", object: DialogInterface.OnClickListener {
+                    override fun onClick(dialog:DialogInterface, id:Int) {}
+                })
+                .setNegativeButton("MANUAL", object: DialogInterface.OnClickListener {
+                    override fun onClick(dialog:DialogInterface, id:Int) {}
+                })
+            return builder.create()
+        }
+    }
+}
