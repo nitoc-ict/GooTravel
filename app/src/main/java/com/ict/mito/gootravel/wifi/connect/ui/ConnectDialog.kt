6e18cf635ad8a71f6c0abe905d609a17fdffb115
tapokaφ <52156547+Tapioka10315858@users.noreply.github.com>
2019-09-13 16:12:05 +0900

    9行目:の前にも空白入れた15,18行目:の前の空白をなくした

--- app/src/main/java/com/ict/mito/gootravel/wifi/connect/ui/ConnectDialog.kt
+++ app/src/main/java/com/ict/mito/gootravel/wifi/connect/ui/ConnectDialog.kt
@@ -6,16 +6,16 @@ import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 
-class ConnectDialog: DialogFragment() {
+class ConnectDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity)
         builder.setTitle("SSID")
             .setMessage("massage")
             .setPositiveButton("CONNECT", object : DialogInterface.OnClickListener {
-                override fun onClick(dialog : DialogInterface, id : Int) {}
+                override fun onClick(dialog: DialogInterface, id: Int) {}
             })
             .setNegativeButton("CANCEL", object : DialogInterface.OnClickListener {
-                override fun onClick(dialog : DialogInterface, id : Int) {}
+                override fun onClick(dialog: DialogInterface, id: Int) {}
             })
         return builder.create()
     }
