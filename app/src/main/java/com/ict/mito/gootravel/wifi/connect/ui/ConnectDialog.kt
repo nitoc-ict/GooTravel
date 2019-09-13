14adf11133aaa6e437c70b242121a0e100838b46
mitohato <mitohato14@gmail.com>
2019-09-13 15:54:27 +0900

    Remove error

--- app/src/main/java/com/ict/mito/gootravel/wifi/connect/ui/ConnectDialog.kt
+++ app/src/main/java/com/ict/mito/gootravel/wifi/connect/ui/ConnectDialog.kt
@@ -3,8 +3,4 @@ package com.ict.mito.gootravel.wifi.connect.ui
 /**
  * Created by Tapioka10315858 on 2019-09-13.
  */
-class ConnectDialog.kt
-© 2019 GitHub, Inc.
-
-    © 2019 GitHub, Inc.
-    Term
+class ConnectDialog
