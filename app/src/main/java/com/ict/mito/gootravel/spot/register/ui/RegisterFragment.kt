584dec5f2797b2ed78bc9ffd2fc1e4537ec31c5f
uehara1107 <u.sumika1107@gmail.com>
2019-09-16 09:16:43 +0900

    TimePickerDialogできた

--- app/src/main/java/com/ict/mito/gootravel/spot/register/ui/RegisterFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/ui/RegisterFragment.kt
@@ -1,6 +1,9 @@
 package com.ict.mito.gootravel.spot.register.ui
 
+import android.app.Dialog
+import android.app.TimePickerDialog
 import android.content.ContentValues.TAG
+import android.content.Context
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
@@ -8,10 +11,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TimePicker
 import android.widget.Toast
+import androidx.fragment.app.DialogFragment
 
 import com.ict.mito.gootravel.R
 import kotlinx.android.synthetic.main.register_fragment.*
+import java.text.DateFormat
+import java.util.*
+import kotlin.coroutines.coroutineContext
 
 class RegisterFragment : Fragment() {
 
@@ -36,20 +44,34 @@ class RegisterFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         SignIn.setOnClickListener{
-            var name: String = Name.text.toString()
-            var memo: String = Memo.text.toString()
+            var name: String = NameInput.text.toString()
+            var memo: String = MemoInput.text.toString()
 
-            if(Name.length() != 0 && Memo.length() != 0){
+            if(NameInput.length() != 0 && MemoInput.length() != 0){
 
                 Log.d("memo", memo)
                 Log.d("name", name)
-                Name.setText("")
-                Memo.setText("")
+                NameInput.setText("")
+                MemoInput.setText("")
                 val toast = Toast.makeText(context, "入力が完了しました", Toast.LENGTH_SHORT)
                 toast.show()
             }
         }
-    }
 
+        TimePicker.setOnClickListener(object:View.OnClickListener {
+          override  fun onClick(v:View) {
+                val calendar = Calendar.getInstance()
+                val hour = calendar.get(Calendar.HOUR_OF_DAY)
+                val minute = calendar.get(Calendar.MINUTE)
+                val dialog = TimePickerDialog(
+                    context,
+                    object:TimePickerDialog.OnTimeSetListener {
+                        override fun onTimeSet(view:TimePicker, hourOfDay:Int, minute:Int) {}
+                    },
+                    hour, minute, true)
+                dialog.show()
+            }
+        })
+    }
 
 }
