58fcd045e61790ef414a577b4c9d67f3e98427ae
mitohato <mitohato14@gmail.com>
2019-09-16 16:48:07 +0900

    Add test class

---
+++ app/src/test/java/com/ict/mito/gootravel/util/CalcKtTest.kt
@@ -0,0 +1,35 @@
+package com.ict.mito.gootravel.util
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by mitohato14 on 2019-09-16.
+ */
+class CalcKtTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun calcDirectDistance() {
+    }
+
+    @Test
+    fun calcDirection() {
+    }
+
+    @Test
+    fun rad2deg() {
+    }
+
+    @Test
+    fun normalizeRange() {
+    }
+}
