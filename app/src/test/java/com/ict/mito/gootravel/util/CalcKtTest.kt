63674e1e7fd82d4d3cb29783dc85629060cfbe46
mitohato <mitohato14@gmail.com>
2019-09-17 09:14:15 +0900

    Add rad2deg test

--- app/src/test/java/com/ict/mito/gootravel/util/CalcKtTest.kt
+++ app/src/test/java/com/ict/mito/gootravel/util/CalcKtTest.kt
@@ -1,6 +1,7 @@
 package com.ict.mito.gootravel.util
 
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -27,6 +28,26 @@ class CalcKtTest {
 
     @Test
     fun rad2deg() {
+        val actual1 = rad2deg(1f)
+        assertEquals(
+            57.29578f,
+            actual1
+        )
+        val actual2 = rad2deg(0f)
+        assertEquals(
+            0f,
+            actual2
+        )
+        val actual3 = rad2deg(2f)
+        assertEquals(
+            114.59156f,
+            actual3
+        )
+        val actual4 = rad2deg(-1f)
+        assertEquals(
+            -57.29578f,
+            actual4
+        )
     }
 
     @Test
