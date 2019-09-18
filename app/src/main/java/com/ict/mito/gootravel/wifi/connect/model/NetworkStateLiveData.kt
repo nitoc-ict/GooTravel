397bb4441a92295cfb60f98bff7c3604fd41ee5e
mitohato <mitohato14@gmail.com>
2019-09-18 21:32:28 +0900

    Add parent

--- app/src/main/java/com/ict/mito/gootravel/wifi/connect/model/NetworkStateLiveData.kt
+++ app/src/main/java/com/ict/mito/gootravel/wifi/connect/model/NetworkStateLiveData.kt
@@ -1,7 +1,9 @@
 package com.ict.mito.gootravel.wifi.connect.model
 
+import androidx.lifecycle.LiveData
+
 /**
  * Created by mitohato14 on 2019-09-18.
  */
-class NetworkStateLiveData {
+class NetworkStateLiveData : LiveData<Boolean>() {
 }
