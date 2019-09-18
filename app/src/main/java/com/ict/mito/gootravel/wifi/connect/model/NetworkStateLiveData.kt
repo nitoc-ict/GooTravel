a52606f90b1daf62da1790bff5f2fc4e46eece71
mitohato <mitohato14@gmail.com>
2019-09-18 21:35:03 +0900

    Add connectivity managar

--- app/src/main/java/com/ict/mito/gootravel/wifi/connect/model/NetworkStateLiveData.kt
+++ app/src/main/java/com/ict/mito/gootravel/wifi/connect/model/NetworkStateLiveData.kt
@@ -1,10 +1,13 @@
 package com.ict.mito.gootravel.wifi.connect.model
 
 import android.content.Context
+import android.net.ConnectivityManager
 import androidx.lifecycle.LiveData
 
 /**
  * Created by mitohato14 on 2019-09-18.
  */
 class NetworkStateLiveData(private val context: Context) : LiveData<Boolean>() {
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 }
