405274d9f7ab4c0f0f90d66be7fe96b535171412
mitohato <mitohato14@gmail.com>
2019-09-18 09:28:06 +0900

    Ktlint

--- app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
+++ app/src/main/java/com/ict/mito/gootravel/disaster/notification/ui/NotificationAlertDialog.kt
@@ -7,17 +7,17 @@ import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import com.ict.mito.gootravel.R
 
-class NotificationAlertDialog: DialogFragment() {
+class NotificationAlertDialog : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = AlertDialog.Builder(activity)
         builder.setTitle("Earthquake")
             .setMessage("massage")
             .setIcon(R.mipmap.ic_launcher)
             .setPositiveButton("REFUGE", object : DialogInterface.OnClickListener {
-                override fun onClick(dialog : DialogInterface, id : Int) {}
+                override fun onClick(dialog: DialogInterface, id: Int) {}
             })
             .setNegativeButton("MANUAL", object : DialogInterface.OnClickListener {
-                override fun onClick(dialog : DialogInterface, id : Int) {}
+                override fun onClick(dialog: DialogInterface, id: Int) {}
             })
         return builder.create()
     }
