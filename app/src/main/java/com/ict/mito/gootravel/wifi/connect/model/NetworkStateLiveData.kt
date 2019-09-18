383d10aa60bc2d05f88594a253421f7923bef343
mitohato <mitohato14@gmail.com>
2019-09-18 21:44:02 +0900

    Remove member

--- app/src/main/java/com/ict/mito/gootravel/wifi/connect/model/NetworkStateLiveData.kt
+++ app/src/main/java/com/ict/mito/gootravel/wifi/connect/model/NetworkStateLiveData.kt
@@ -10,7 +10,7 @@ import androidx.lifecycle.LiveData
 /**
  * Created by mitohato14 on 2019-09-18.
  */
-class NetworkStateLiveData(private val context: Context) : LiveData<Boolean>() {
+class NetworkStateLiveData(context: Context) : LiveData<Boolean>() {
     private val connectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
