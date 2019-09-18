a5a9162c8ec82a09e68cb763bf783b5734f6c287
mitohato <mitohato14@gmail.com>
2019-09-18 21:32:55 +0900

    Add context para

--- app/src/main/java/com/ict/mito/gootravel/wifi/connect/model/NetworkStateLiveData.kt
+++ app/src/main/java/com/ict/mito/gootravel/wifi/connect/model/NetworkStateLiveData.kt
@@ -1,9 +1,10 @@
 package com.ict.mito.gootravel.wifi.connect.model
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 
 /**
  * Created by mitohato14 on 2019-09-18.
  */
-class NetworkStateLiveData : LiveData<Boolean>() {
+class NetworkStateLiveData(private val context: Context) : LiveData<Boolean>() {
 }
