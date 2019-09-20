ef243fdac1a7ff68f7e6893deb57a16f0bdad44d
mitohato <mitohato14@gmail.com>
2019-09-20 11:34:02 +0900

    Add fragment for language setting

---
+++ app/src/main/java/com/ict/mito/gootravel/setting/language/ui/LanguageSettingFragment.kt
@@ -0,0 +1,36 @@
+package com.ict.mito.gootravel.setting.language.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.ict.mito.gootravel.R
+
+class LanguageSettingFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LanguageSettingFragment()
+    }
+
+    private lateinit var viewModel: LanguageSettingViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.language_setting_fragment,
+            container,
+            false
+        )
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(LanguageSettingViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
