83ae6a1e1f93cd1efd1a49829375236212526d9e
mitohato <mitohato14@gmail.com>
2019-09-21 22:21:50 +0900

    Add method what move  spotdata from xsv to room

--- app/src/main/java/com/ict/mito/gootravel/spot/activity/SpotViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/activity/SpotViewModel.kt
@@ -2,9 +2,20 @@ package com.ict.mito.gootravel.spot.activity
 
 import androidx.lifecycle.ViewModel
 import com.ict.mito.gootravel.repo.Repository
+import io.reactivex.rxkotlin.subscribeBy
 
 /**
  * Created by mitohato14 on 2019-09-21.
  */
 class SpotViewModel(private val repository: Repository) : ViewModel() {
+
+    private fun addSpotDataToRoomFromCSV() {
+        repository.getSpotDataByCSV().subscribeBy(
+            onSuccess = { spots ->
+                spots.forEach {
+                    repository.add(it)
+                }
+            }
+        )
+    }
 }
