90c53c122e2ffda965ddceeed7d18a09b5a8cc34
mitohato <mitohato14@gmail.com>
2019-09-21 22:06:58 +0900

    Add viewmodel parent

--- app/src/main/java/com/ict/mito/gootravel/spot/activity/SpotViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/activity/SpotViewModel.kt
@@ -1,7 +1,9 @@
 package com.ict.mito.gootravel.spot.activity
 
+import androidx.lifecycle.ViewModel
+
 /**
  * Created by mitohato14 on 2019-09-21.
  */
-class SpotViewModel {
+class SpotViewModel : ViewModel() {
 }
