258acc518375a0d3c138217877b211597f25b6e2
mitohato <mitohato14@gmail.com>
2019-09-21 22:12:12 +0900

    Add repo para

--- app/src/main/java/com/ict/mito/gootravel/spot/activity/SpotViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/activity/SpotViewModel.kt
@@ -1,9 +1,10 @@
 package com.ict.mito.gootravel.spot.activity
 
 import androidx.lifecycle.ViewModel
+import com.ict.mito.gootravel.repo.Repository
 
 /**
  * Created by mitohato14 on 2019-09-21.
  */
-class SpotViewModel : ViewModel() {
+class SpotViewModel(private val repository: Repository) : ViewModel() {
 }
