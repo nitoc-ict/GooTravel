5b5f642e61c97c59314d1c2bed7de2aa25250c23
mitohato <mitohato14@gmail.com>
2019-09-23 11:26:18 +0900

    Get all spot data

--- app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/RadarViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/RadarViewModel.kt
@@ -5,9 +5,21 @@ import android.view.View
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModel
 import com.ict.mito.gootravel.repo.Repository
+import com.ict.mito.gootravel.spot.model.SpotData
 import com.ict.mito.gootravel.spot.select.radar.ui.dialog.SelectSpotBottomSheetFragment
+import io.reactivex.rxkotlin.subscribeBy
 
 class RadarViewModel(private val repository: Repository) : ViewModel() {
+    private var spotdataList: List<SpotData> = listOf()
+
+    init {
+        repository.getAllSpotData().subscribeBy(
+            onSuccess = {
+                spotdataList = it
+            }
+        )
+    }
+
     lateinit var fragmentManager: FragmentManager
 
     fun onClickSpot(view: View) {
