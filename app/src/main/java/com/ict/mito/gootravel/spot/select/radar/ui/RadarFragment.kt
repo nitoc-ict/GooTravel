e6a065ec06faffe244f3f24737c31a8c85c96369
mitohato <mitohato14@gmail.com>
2019-09-23 22:03:06 +0900

    Set icon logic in progress

--- app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/RadarFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/RadarFragment.kt
@@ -5,7 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -14,6 +17,7 @@ import com.ict.mito.gootravel.databinding.RadarFragmentBinding
 import com.ict.mito.gootravel.disaster.manual.ui.ManualActivity
 import com.ict.mito.gootravel.setting.activity.SettingActivity
 import kotlinx.android.synthetic.main.activity_spot.*
+import org.jetbrains.anko.dip
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RadarFragment : Fragment() {
@@ -83,6 +87,40 @@ class RadarFragment : Fragment() {
                 viewModel.onClickSpot(view)
             }
             it.lifecycleOwner = this
+        }
+
+        val constraintLayout: ConstraintLayout = binding?.root as ConstraintLayout
+        val constraintSet =  ConstraintSet()
+        constraintSet.clone(constraintLayout)
+
+        val spotButton = Button(context)
+        spotButton.apply {
+            layoutParams = ConstraintLayout.LayoutParams(dip(30), dip(30))
+            id = 1
+            setBackgroundResource(R.drawable.wifi_spot)
+            constraintLayout.addView(spotButton)
+            constraintSet.connect(
+                1,
+                ConstraintSet.START,
+                R.id.wifi_spot,
+                ConstraintSet.END
+            )
+            constraintSet.connect(
+                1,
+                ConstraintSet.TOP,
+                R.id.wifi_spot,
+                ConstraintSet.BOTTOM
+            )
+            constraintSet.applyTo(constraintLayout)
+//        viewModel.spotdataList.forEach {
+//            val spotButton = Button(context)
+//            spotButton.apply {
+//                id = it.id.toInt()
+//                width = dip(10)
+//                height = dip(10)
+//                setBackgroundResource(R.drawable.wifi_spot)
+//                constraintLayout.addView(spotButton)
+//            }
         }
 
         return binding?.root
