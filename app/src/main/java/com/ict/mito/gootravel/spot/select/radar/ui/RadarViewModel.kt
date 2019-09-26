9458ea29aa51a4e66f0f8eff3cdce7c308093729
mitohato <mitohato14@gmail.com>
2019-09-26 15:38:45 +0900

    Add livedata to constructor

--- app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/RadarViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/radar/ui/RadarViewModel.kt
@@ -5,11 +5,17 @@ import android.view.View
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModel
 import com.ict.mito.gootravel.repo.Repository
+import com.ict.mito.gootravel.spot.model.LocationLiveData
+import com.ict.mito.gootravel.spot.model.OrientationLiveData
 import com.ict.mito.gootravel.spot.model.SpotData
 import com.ict.mito.gootravel.spot.select.radar.ui.dialog.SelectSpotBottomSheetFragment
 import io.reactivex.rxkotlin.subscribeBy
 
-class RadarViewModel(private val repository: Repository) : ViewModel() {
+class RadarViewModel(
+    private val repository: Repository,
+    private val orientationLiveData: OrientationLiveData,
+    private val locationLiveData: LocationLiveData
+) : ViewModel() {
     var spotdataList: List<SpotData> = listOf()
 
     init {
