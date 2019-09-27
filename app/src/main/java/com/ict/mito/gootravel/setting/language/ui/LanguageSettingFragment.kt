636520e2fd0c9000627af4832cf04049a78e4302
tapioka10315858 <tapiokamai10315858@gmail.com>
2019-09-27 20:53:48 +0900

    ListViewを作成した

--- app/src/main/java/com/ict/mito/gootravel/setting/language/ui/LanguageSettingFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/setting/language/ui/LanguageSettingFragment.kt
@@ -7,6 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.ict.mito.gootravel.R
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ListView
+import android.app.Activity
+import androidx.databinding.DataBindingUtil.setContentView
+
 
 class LanguageSettingFragment : Fragment() {
 
@@ -18,7 +24,7 @@ class LanguageSettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(
-            R.layout.language_setting_fragment,
+            com.ict.mito.gootravel.R.layout.language_setting_fragment,
             container,
             false
         )
@@ -28,5 +34,17 @@ class LanguageSettingFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(LanguageSettingViewModel::class.java)
         // TODO: Use the ViewModel
+        class LanguageSettingFragment : Fragment() {
+            public override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+
+                setContentView(com.ict.mito.gootravel.R.layout.language_setting_fragment)
+            }
+
+            private fun setContentView(languageSettingFragment: Int) {
+
+
+            }
+        }
     }
 }
