aca34bc70a3fd10d422ac9b0bedae9097c8c9ec8
tapioka10315858 <tapiokamai10315858@gmail.com>
2019-09-29 23:20:27 +0900

    RecyclerViewのAdapterを作った

--- app/src/main/java/com/ict/mito/gootravel/setting/language/ui/CustomAdapter.kt
+++ app/src/main/java/com/ict/mito/gootravel/setting/language/ui/CustomAdapter.kt
@@ -1,4 +1,31 @@
 package com.ict.mito.gootravel.setting.language.ui
 
-class CustomAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ict.mito.gootravel.R
+
+class CustomAdapter(val languageList: ArrayList<language>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.list_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: CustomAdapter.ViewHolder, position: Int) {
+        holder.bindItems(languageList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return languageList.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindItems(language: language) {
+            val textViewName = itemView.findViewById(R.id.textViewlanguage) as TextView
+            textViewName.text = language.name
+        }
+    }
 }
