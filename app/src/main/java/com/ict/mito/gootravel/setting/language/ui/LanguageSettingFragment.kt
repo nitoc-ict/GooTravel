eb68bfd978a3b2fc8715756bb32deb7ae4c16181
tapioka10315858 <tapiokamai10315858@gmail.com>
2019-10-02 18:12:52 +0900

    アドバイスをもとにコードを書き直した

--- app/src/main/java/com/ict/mito/gootravel/setting/language/ui/LanguageSettingFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/setting/language/ui/LanguageSettingFragment.kt
@@ -1,15 +1,10 @@
 package com.ict.mito.gootravel.setting.language.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ict.mito.gootravel.R
 
@@ -22,52 +17,18 @@ class LanguageSettingFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) {
-        fun setContentView(languageSettingFragment: Int) {
+    ): View? {
+        val recyclerView = (R.id.recyclerView) as RecyclerView
 
+        val languages = ArrayList<language>()
 
-        }
+        languages.add(language("英語"))
+        languages.add(language("中国語(繫体字)"))
+        languages.add(language("中国語(簡体字)"))
+        languages.add(language("韓国語"))
 
-        fun findViewById(recyclerView: Int) {
+        val adapter = CustomAdapter(languages)
 
-
-        }
-
-        fun LinearLayoutManager(
-            languageSettingFragment: LanguageSettingFragment,
-            vertical: Int,
-            b: Boolean
-        ) {
-
-
-        }
-
-        @SuppressLint("WrongConstant")
-        fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.language_setting_fragment)
-
-            val recyclerView = findViewById(R.id.recyclerView) as RecyclerView
-
-            recyclerView.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
-
-            val languages = ArrayList<language>()
-
-            languages.add(language("英語"))
-            languages.add(language("中国語(繫体字)"))
-            languages.add(language("中国語(簡体字)"))
-            languages.add(language("韓国語"))
-
-            val adapter = CustomAdapter(languages)
-
-            recyclerView.adapter = adapter
-        }
-
-        override fun onActivityCreated(savedInstanceState: Bundle?) {
-            super.onActivityCreated(savedInstanceState)
-            viewModel = ViewModelProviders.of(this).get(LanguageSettingViewModel::class.java)
-            // TODO: Use the ViewModel
-
-        }
+        recyclerView.adapter = adapter
     }
 }
