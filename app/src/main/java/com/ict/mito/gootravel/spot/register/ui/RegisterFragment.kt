72cbf94071d8fbe8a155f5abcfb86e1894e4083c
mitohato <mitohato14@gmail.com>
2019-10-03 13:04:53 +0900

    Set title and back button

--- app/src/main/java/com/ict/mito/gootravel/spot/register/ui/RegisterFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/ui/RegisterFragment.kt
@@ -8,6 +8,7 @@ import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -90,6 +91,13 @@ class RegisterFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         activity?.bottom_appbar?.replaceMenu(R.menu.empty_menu)
+
+        val appCompatActivity = activity as AppCompatActivity?
+        appCompatActivity?.supportActionBar?.let {
+            it.title = getString(R.string.register_fragment_title)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeButtonEnabled(true)
+        }
     }
 
     override fun onDestroy() {
