7c00cd08a7f44dbc10460c31a8e11eeb775af041
mitohato <mitohato14@gmail.com>
2019-10-03 22:56:22 +0900

    Add constructor and parent

--- app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListViewHolder.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListViewHolder.kt
@@ -1,7 +1,11 @@
 package com.ict.mito.gootravel.spot.register.list
 
+import androidx.recyclerview.widget.RecyclerView
+import com.ict.mito.gootravel.databinding.RegisterSpotListRowBinding
+
 /**
  * Created by mitohato14 on 2019-10-03.
  */
-class RegisterSpotListViewHolder {
+class RegisterSpotListViewHolder(private val binding: RegisterSpotListRowBinding) :
+    RecyclerView.ViewHolder(binding.root) {
 }
