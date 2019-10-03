5c325e763ea1e96b1599648a55d51276b1b7ce0f
mitohato <mitohato14@gmail.com>
2019-10-03 23:03:22 +0900

    Impl adapter method

--- app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListAdapter.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListAdapter.kt
@@ -1,5 +1,7 @@
 package com.ict.mito.gootravel.spot.register.list
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.ict.mito.gootravel.spot.model.RegisterPointData
 
@@ -8,4 +10,21 @@ import com.ict.mito.gootravel.spot.model.RegisterPointData
  */
 class RegisterSpotListAdapter(private val registerSpotList: List<RegisterPointData>) :
     RecyclerView.Adapter<RegisterSpotListViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RegisterSpotListViewHolder = RegisterSpotListViewHolder.create(
+        LayoutInflater.from(parent.context),
+        parent,
+        false
+    )
+
+    override fun getItemCount(): Int = registerSpotList.size
+
+    override fun onBindViewHolder(
+        holder: RegisterSpotListViewHolder,
+        position: Int
+    ) {
+        holder.bind(registerSpotList[position])
+    }
 }
