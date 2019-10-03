74a6949472564005f74e6ae211fde27fe637a39c
mitohato <mitohato14@gmail.com>
2019-10-03 23:07:25 +0900

    Change list type

--- app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListViewModel.kt
@@ -1,9 +1,11 @@
 package com.ict.mito.gootravel.spot.register.list
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.ict.mito.gootravel.spot.model.RegisterPointData
 
 class RegisterSpotListViewModel : ViewModel() {
-    private lateinit var registerSpotList: List<RegisterPointData>
-    var adapter: RegisterSpotListAdapter = RegisterSpotListAdapter(registerSpotList)
+    private var registerSpotList: MutableLiveData<List<RegisterPointData>> = MutableLiveData()
+    var adapter: RegisterSpotListAdapter =
+        RegisterSpotListAdapter(registerSpotList.value ?: listOf())
 }
