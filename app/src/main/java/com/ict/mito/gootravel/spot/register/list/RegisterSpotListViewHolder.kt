5e6328b5350bab316f1562c3c0a1979661b07ca8
mitohato <mitohato14@gmail.com>
2019-10-03 22:58:16 +0900

    Add bind method

--- app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListViewHolder.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListViewHolder.kt
@@ -2,10 +2,18 @@ package com.ict.mito.gootravel.spot.register.list
 
 import androidx.recyclerview.widget.RecyclerView
 import com.ict.mito.gootravel.databinding.RegisterSpotListRowBinding
+import com.ict.mito.gootravel.spot.model.RegisterPointData
 
 /**
  * Created by mitohato14 on 2019-10-03.
  */
 class RegisterSpotListViewHolder(private val binding: RegisterSpotListRowBinding) :
     RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(registerPointData: RegisterPointData) {
+        binding.also {
+            it.registerPoint = registerPointData
+            it.notifyChange()
+        }
+    }
 }
