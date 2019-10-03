264bc11093ee0887139513c12c8fa458645bff92
mitohato <mitohato14@gmail.com>
2019-10-03 23:39:03 +0900

    Add init logic

--- app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListViewModel.kt
@@ -2,11 +2,21 @@ package com.ict.mito.gootravel.spot.register.list
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ict.mito.gootravel.db.DataBaseConverter
 import com.ict.mito.gootravel.repo.Repository
 import com.ict.mito.gootravel.spot.model.RegisterPointData
+import io.reactivex.rxkotlin.subscribeBy
 
 class RegisterSpotListViewModel(private val repository: Repository) : ViewModel() {
     private var registerSpotList: MutableLiveData<List<RegisterPointData>> = MutableLiveData()
     var adapter: RegisterSpotListAdapter =
         RegisterSpotListAdapter(registerSpotList.value ?: listOf())
+
+    init {
+        repository.getAllRegisterLocation().subscribeBy(
+            onSuccess = {
+                registerSpotList.postValue(DataBaseConverter().convert2RegisterPointDataList(it))
+            }
+        )
+    }
 }
