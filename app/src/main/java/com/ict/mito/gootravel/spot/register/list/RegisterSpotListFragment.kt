4baaa9324e862c215fb8cc383e94ba6491359ea2
mitohato <mitohato14@gmail.com>
2019-10-03 22:22:18 +0900

    Add binding value

--- app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListFragment.kt
@@ -4,22 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.ict.mito.gootravel.R
+import com.ict.mito.gootravel.databinding.RegisterSpotListFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RegisterSpotListFragment : Fragment() {
     private val viewmodel: RegisterSpotListViewModel by viewModel()
+    private var binding: RegisterSpotListFragmentBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(
+        binding = DataBindingUtil.inflate(
+            inflater,
             R.layout.register_spot_list_fragment,
             container,
             false
         )
+
+        return binding?.root
     }
 }
