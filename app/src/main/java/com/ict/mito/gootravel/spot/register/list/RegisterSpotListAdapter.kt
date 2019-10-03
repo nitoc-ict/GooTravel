9c1dfa78ef4007f83370778cb8bfb7fa638d131e
mitohato <mitohato14@gmail.com>
2019-10-03 23:01:40 +0900

    Add constructor and parent

--- app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListAdapter.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListAdapter.kt
@@ -1,7 +1,11 @@
 package com.ict.mito.gootravel.spot.register.list
 
+import androidx.recyclerview.widget.RecyclerView
+import com.ict.mito.gootravel.spot.model.RegisterPointData
+
 /**
  * Created by mitohato14 on 2019-10-03.
  */
-class RegisterSpotListAdapter {
+class RegisterSpotListAdapter(private val registerSpotList: List<RegisterPointData>) :
+    RecyclerView.Adapter<RegisterSpotListViewHolder>() {
 }
