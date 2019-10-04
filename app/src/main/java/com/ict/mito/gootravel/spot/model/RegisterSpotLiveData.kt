4c3495cd4b38c384fe015cc5e210e6bb0486b763
mitohato <mitohato14@gmail.com>
2019-10-04 13:46:34 +0900

    Add livedata class

---
+++ app/src/main/java/com/ict/mito/gootravel/spot/model/RegisterSpotLiveData.kt
@@ -0,0 +1,9 @@
+package com.ict.mito.gootravel.spot.model
+
+import androidx.lifecycle.MutableLiveData
+
+/**
+ * Created by mitohato14 on 2019-10-04.
+ */
+class RegisterSpotLiveData : MutableLiveData<RegisterPointData>() {
+}
