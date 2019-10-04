a3f7b723fc4f59645b87a8809447a565929611a0
mitohato <mitohato14@gmail.com>
2019-10-04 13:47:52 +0900

    Impl active method

--- app/src/main/java/com/ict/mito/gootravel/spot/model/RegisterSpotLiveData.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/model/RegisterSpotLiveData.kt
@@ -1,9 +1,27 @@
 package com.ict.mito.gootravel.spot.model
 
+import android.content.Context
+import android.graphics.BitmapFactory
 import androidx.lifecycle.MutableLiveData
+import com.ict.mito.gootravel.R
 
 /**
  * Created by mitohato14 on 2019-10-04.
  */
-class RegisterSpotLiveData : MutableLiveData<RegisterPointData>() {
+class RegisterSpotLiveData(private val context: Context) : MutableLiveData<RegisterPointData>() {
+    override fun onActive() {
+        super.onActive()
+        value = RegisterPointData(
+            id = 0,
+            latitude = 0.0,
+            longitude = 0.0,
+            name = "",
+            memo = "",
+            notificationTime = 0,
+            spotBitmap = BitmapFactory.decodeResource(
+                context.resources,
+                R.drawable.ic_location_on_black_36dp
+            )
+        )
+    }
 }
