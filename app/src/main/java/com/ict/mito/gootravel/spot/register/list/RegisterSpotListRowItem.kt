92975c9dcbde09e80f2415f11d9024641c8cc6e4
mitohato <mitohato14@gmail.com>
2019-10-05 22:56:33 +0900

    Add constructor

--- app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListRowItem.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListRowItem.kt
@@ -1,10 +1,12 @@
 package com.ict.mito.gootravel.spot.register.list
 
 import com.ict.mito.gootravel.databinding.RegisterSpotListRowBinding
+import com.ict.mito.gootravel.spot.model.RegisterPointData
 import com.xwray.groupie.databinding.BindableItem
 
 /**
  * Created by mitohato14 on 2019-10-05.
  */
-class RegisterSpotListRowItem : BindableItem<RegisterSpotListRowBinding>() {
+class RegisterSpotListRowItem(val registerSpotData: RegisterPointData) :
+    BindableItem<RegisterSpotListRowBinding>() {
 }
