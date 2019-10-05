79325e40ddaced7df7307f839cdf03f0d1dc4c3b
mitohato <mitohato14@gmail.com>
2019-10-05 22:55:26 +0900

    Add row item class

---
+++ app/src/main/java/com/ict/mito/gootravel/spot/register/list/RegisterSpotListRowItem.kt
@@ -0,0 +1,10 @@
+package com.ict.mito.gootravel.spot.register.list
+
+import com.ict.mito.gootravel.databinding.RegisterSpotListRowBinding
+import com.xwray.groupie.databinding.BindableItem
+
+/**
+ * Created by mitohato14 on 2019-10-05.
+ */
+class RegisterSpotListRowItem : BindableItem<RegisterSpotListRowBinding>() {
+}
