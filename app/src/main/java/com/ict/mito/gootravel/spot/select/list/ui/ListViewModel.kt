6db55953693674640d685ba562dd11edce3c8749
mitohato <mitohato14@gmail.com>
2019-10-07 21:30:20 +0900

    Remove unuse code

--- app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListViewModel.kt
@@ -3,10 +3,8 @@ package com.ict.mito.gootravel.spot.select.list.ui
 import androidx.lifecycle.ViewModel
 
 class ListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
 
-    private var i : Int = 20
-    var listItems : List<ListRowItem> = getItems(i)
+    var listItems : List<ListRowItem> = listOf()
     val adapter: ListViewAdapter = ListViewAdapter(listItems)
 
     private fun getItems(i : Int) : List<ListRowItem> {
