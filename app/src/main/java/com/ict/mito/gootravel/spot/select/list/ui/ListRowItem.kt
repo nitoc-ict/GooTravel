f0e4289e0fee3d6d7c6d913b740088a8fb222edf
mitohato <mitohato14@gmail.com>
2019-10-07 21:57:59 +0900

    Impl get layout method

--- app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListRowItem.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListRowItem.kt
@@ -1,6 +1,7 @@
 package com.ict.mito.gootravel.spot.select.list.ui
 
 import androidx.navigation.NavController
+import com.ict.mito.gootravel.R
 import com.ict.mito.gootravel.databinding.ListRowItemBinding
 import com.ict.mito.gootravel.spot.model.WiFiSpotListItem
 import com.xwray.groupie.databinding.BindableItem
@@ -12,9 +13,7 @@ class ListRowItem(
     private val wiFiSpotListItem: WiFiSpotListItem,
     private val navController: NavController
 ) : BindableItem<ListRowItemBinding>() {
-    override fun getLayout(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getLayout(): Int = R.layout.list_row_item
 
     override fun bind(viewBinding: ListRowItemBinding, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
