fee04bc862124fded28d31ec89d7f0192fe86ce7
mitohato <mitohato14@gmail.com>
2019-10-07 21:32:41 +0900

    Add list variable

--- app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListViewModel.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListViewModel.kt
@@ -1,9 +1,11 @@
 package com.ict.mito.gootravel.spot.select.list.ui
 
 import androidx.lifecycle.ViewModel
+import com.ict.mito.gootravel.spot.model.SpotData
 
 class ListViewModel : ViewModel() {
 
+    var spotdataList: List<SpotData> = listOf()
     var listItems : List<ListRowItem> = listOf()
     val adapter: ListViewAdapter = ListViewAdapter(listItems)
 
