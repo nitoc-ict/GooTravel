4f992221b48844a51a2598d9b5f06f010a5475b9
mitohato <mitohato14@gmail.com>
2019-10-07 21:57:19 +0900

    Add bindable parent

--- app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListRowItem.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/list/ui/ListRowItem.kt
@@ -1,7 +1,9 @@
 package com.ict.mito.gootravel.spot.select.list.ui
 
 import androidx.navigation.NavController
+import com.ict.mito.gootravel.databinding.ListRowItemBinding
 import com.ict.mito.gootravel.spot.model.WiFiSpotListItem
+import com.xwray.groupie.databinding.BindableItem
 
 /**
  * Created by mitohato14 on 2019-07-24.
@@ -9,4 +11,4 @@ import com.ict.mito.gootravel.spot.model.WiFiSpotListItem
 class ListRowItem(
     private val wiFiSpotListItem: WiFiSpotListItem,
     private val navController: NavController
-)
+) : BindableItem<ListRowItemBinding>()
