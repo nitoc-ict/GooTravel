c94abb2c8d4af8b657a6273d544c781b6cf85140
mitohato <mitohato14@gmail.com>
2019-10-10 21:28:08 +0900

    move

--- app/src/main/java/com/ict/mito/gootravel/spot/select/search/ui/SearchFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/search/ui/SearchFragment.kt
@@ -20,6 +20,36 @@ class SearchFragment : Fragment() {
     private val viewModel: SearchViewModel by viewModel()
 
     private val onMenuItemClickListener = Toolbar.OnMenuItemClickListener { menu ->
+        when (menu.itemId) {
+            R.id.appbar_radar -> {
+                findNavController().navigate(R.id.action_searchFragment_to_radarFragment)
+            }
+            R.id.appbar_list -> {
+                findNavController().navigate(R.id.action_searchFragment_to_listFragment)
+            }
+            R.id.app_bar_manual -> {
+                startActivity(
+                    Intent(
+                        context,
+                        ManualActivity::class.java
+                    )
+                )
+            }
+            R.id.app_bar_language -> {
+                startActivity(
+                    Intent(
+                        context,
+                        SettingActivity::class.java
+                    )
+                )
+            }
+            R.id.app_bar_register -> {
+                findNavController().navigate(
+                    R.id.action_searchFragment_to_registerSpotListFragment
+                )
+            }
+        }
+        true
     }
 
     override fun onCreateView(
@@ -30,36 +60,6 @@ class SearchFragment : Fragment() {
         activity?.bottom_appbar?.let {
             it.replaceMenu(R.menu.search_bottomappbar_menu)
             activity?.bottom_appbar?.setOnMenuItemClickListener { menu ->
-                when (menu.itemId) {
-                    R.id.appbar_radar -> {
-                        findNavController().navigate(R.id.action_searchFragment_to_radarFragment)
-                    }
-                    R.id.appbar_list -> {
-                        findNavController().navigate(R.id.action_searchFragment_to_listFragment)
-                    }
-                    R.id.app_bar_manual -> {
-                        startActivity(
-                            Intent(
-                                context,
-                                ManualActivity::class.java
-                            )
-                        )
-                    }
-                    R.id.app_bar_language -> {
-                        startActivity(
-                            Intent(
-                                context,
-                                SettingActivity::class.java
-                            )
-                        )
-                    }
-                    R.id.app_bar_register -> {
-                        findNavController().navigate(
-                            R.id.action_searchFragment_to_registerSpotListFragment
-                        )
-                    }
-                }
-                true
             }
         }
         return inflater.inflate(
