af82586b90e7b948eef5653be2d7b76e5756acd6
mitohato <mitohato14@gmail.com>
2019-10-10 21:32:47 +0900

    Use binding

--- app/src/main/java/com/ict/mito/gootravel/spot/select/search/ui/SearchFragment.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/select/search/ui/SearchFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.ict.mito.gootravel.R
@@ -59,10 +60,13 @@ class SearchFragment : Fragment() {
                 true
             }
         }
-        return inflater.inflate(
+        binding = DataBindingUtil.inflate(
+            inflater,
             R.layout.search_fragment,
             container,
             false
         )
+
+        return binding?.root
     }
 }
