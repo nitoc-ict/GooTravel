a3574e356ea3a94a6ee7e3adfb0d1c041e831061
mitohato <mitohato14@gmail.com>
2019-11-05 22:02:54 +0900

    Set type

--- app/src/main/java/com/ict/mito/gootravel/spot/model/SpotFragmentType.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/model/SpotFragmentType.kt
@@ -3,5 +3,11 @@ package com.ict.mito.gootravel.spot.model
 /**
  * Created by mitohato14 on 2019-11-04.
  */
-enum class SpotFragmentType {
+enum class SpotFragmentType(val title: String) {
+    RADAR("Wi-Fi Spot"),
+    LIST("Wi-Fi Spot"),
+    SEARCH("Search"),
+    NAVIGATE("Navigate"),
+    REGISTER_LIST("Registration Point"),
+    RAGISTER("Registration Point")
 }
