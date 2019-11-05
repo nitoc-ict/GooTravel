1602958a9ae27c7fa65c77b6c6c85ed795871c63
mitohato <mitohato14@gmail.com>
2019-11-05 22:21:13 +0900

    Replace id

--- app/src/main/java/com/ict/mito/gootravel/spot/model/SpotFragmentType.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/model/SpotFragmentType.kt
@@ -1,13 +1,17 @@
 package com.ict.mito.gootravel.spot.model
 
+import com.ict.mito.gootravel.R
+
 /**
  * Created by mitohato14 on 2019-11-04.
  */
-enum class SpotFragmentType(val title: String) {
-    RADAR("Wi-Fi Spot"),
-    LIST("Wi-Fi Spot"),
-    SEARCH("Search"),
-    NAVIGATE("Navigate"),
-    REGISTER_LIST("Registration Point"),
-    RAGISTER("Registration Point")
+enum class SpotFragmentType(
+    val titleId: Int
+) {
+    RADAR(R.string.wifi_spot),
+    LIST(R.string.wifi_spot),
+    SEARCH(R.string.appbar_search_title),
+    NAVIGATE(R.string.navigate),
+    REGISTER_LIST(R.string.register_fragment_title),
+    RAGISTER(R.string.register_fragment_title)
 }
