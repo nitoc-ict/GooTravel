583770d14ae3518e5775faa7f1b5479c4ad3327b
mitohato <mitohato14@gmail.com>
2019-11-05 22:24:40 +0900

    Add

--- app/src/main/java/com/ict/mito/gootravel/spot/model/SpotFragmentType.kt
+++ app/src/main/java/com/ict/mito/gootravel/spot/model/SpotFragmentType.kt
@@ -6,12 +6,38 @@ import com.ict.mito.gootravel.R
  * Created by mitohato14 on 2019-11-04.
  */
 enum class SpotFragmentType(
-    val titleId: Int
+    val titleId: Int,
+    val menuId: Int,
+    val enableBack: Boolean
 ) {
-    RADAR(R.string.wifi_spot),
-    LIST(R.string.wifi_spot),
-    SEARCH(R.string.appbar_search_title),
-    NAVIGATE(R.string.navigate),
-    REGISTER_LIST(R.string.register_fragment_title),
-    RAGISTER(R.string.register_fragment_title)
+    RADAR(
+        R.string.wifi_spot,
+        R.menu.radar_bottomappbar_menu,
+        false
+    ),
+    LIST(
+        R.string.wifi_spot,
+        R.menu.list_bottomappbar_menu,
+        false
+    ),
+    SEARCH(
+        R.string.appbar_search_title,
+        R.menu.search_bottomappbar_menu,
+        false
+    ),
+    NAVIGATE(
+        R.string.navigate,
+        R.menu.empty_menu,
+        true
+    ),
+    REGISTER_LIST(
+        R.string.register_fragment_title,
+        R.menu.empty_menu,
+        true
+    ),
+    RAGISTER(
+        R.string.register_fragment_title,
+        R.menu.empty_menu,
+        true
+    )
 }
